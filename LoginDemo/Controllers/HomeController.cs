f71da9c026c3565c004abf1903b37d62f0f6ad54
Hu Hai <huhai@hantek.com>
2017-07-05 17:03:37 +0800

    可以访问Controller 中的方法

---
+++ LoginDemo/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginDemo.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "我是首页";
+        }
+    }
+}
--- LoginDemo/Startup.cs
+++ LoginDemo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
 
 namespace LoginDemo
 {
@@ -16,6 +17,7 @@ namespace LoginDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +30,19 @@ namespace LoginDemo
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc(ConfigureRoute);
+
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+        }
+
+        private void ConfigureRoute(IRouteBuilder routeBuilder)
+        {
+            //Home/Index , 注意： = 两边不能有空格，否则路由无效。
+            routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
